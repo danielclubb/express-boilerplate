8fbe686ea83441bb88031a9bb9856e4902f87d52
danielclubb <danieljohnclubb@gmail.com>
2022-09-25 15:32:06 +0100

    update

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,12 +1,5 @@
-
-const getPosts = require('./handlers/getPosts');
-const posts = getPosts();
-
 async function homepageController(request, response, next) {
-    const viewContext = {
-        posts: posts
-    };
-    return response.render('homepage', viewContext);
+    return response.render('home');
 }
 
 module.exports = {
